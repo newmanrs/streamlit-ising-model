90a6155b25ffe279946c806693e20d2e752e24f6
newmanrs <newmanrs@umich.edu>
2022-05-01 20:30:43 -0700

    more pythonic formatting and fixed latex equations

--- ising_main.py
+++ ising_main.py
@@ -1,45 +1,50 @@
 import altair as alt
 import numpy as np
-import pandas as pd
 import streamlit as st
 import time
 from ising_state import Ising
 
+
 def make_chart(rect_size=5):
     """
-    Create Altair plot from pd dataframe
+    Build altair heatmap to show simulation
     """
 
     source = Ising.get_plot_data()
-    chart_title = f"Ising State for Nx = {Ising.Nx}, Ny = {Ising.Ny}, sweep = {Ising.sweeps}"
 
+    chart_title = f"Ising State for Nx = {Ising.Nx}, " \
+        f"Ny = {Ising.Ny}, sweep = {Ising.sweeps}"
+
+    # mark_rect <--> heatmap
     chart = alt.Chart(source).mark_rect().encode(
         x='i:O',
         y='j:O',
-        color='spin:Q',
-        tooltip=['i','j','spin','energy']
+        color=alt.Color('spin:Q'),
+        tooltip=['i', 'j', 'spin', 'energy']
     ).properties(
-        height = {'step':rect_size},
-        width  = {'step':rect_size},
-        title = chart_title
-    ).configure_scale(bandPaddingInner=0.1)
-
-    chart.configure_title(
-        fontSize=20,
+        height={'step': rect_size},
+        width={'step': rect_size},
+        title=chart_title
+    ).configure_scale(
+        # Gap between rectangles
+        bandPaddingInner=0.1
+    ).configure_title(
+        fontSize=16,
         anchor='start'
     )
 
     return chart
 
+
 def render_streamlit():
-    st.title("Ising Model")
+    st.title("Square Lattice Ising Model")
 
     st.write(
         r'''
-        Basic 2D Ising lattice Monte Carlo simulator using 4 neighbors and periodic boundary conditions.  The Hamiltonian $H$ is then given then by
+        Basic 2D Ising model Monte Carlo simulator on the square lattice with four adjacent neighbors and periodic boundary conditions.  The Hamiltonian $H$ is then given then by
 
 
-        $\quad H=-\sum_{i,j}\sigma_{i}\sigma_{j}$
+        $\displaystyle H=-\sum_{i=0}^{N_x N_y} \sum_{j=0}^{4} \sigma_{i}\sigma_{j}$
 
         where $\sigma_{i}$ is the spin of site $i$, and this summation is carried out over four adjacent neighbors $j$.
         '''
@@ -59,11 +64,11 @@ def render_streamlit():
         value=25,
         max_value=400)
 
-    T =  st.sidebar.number_input(
+    T = st.sidebar.number_input(
         "T (Temperature)",
-        min_value = 0.0001,
-        value = 2.2692,
-        max_value = 1000.0,
+        min_value=0.0001,
+        value=2.2692,  # Phase transition temp
+        max_value=1000.0,
         format="%.4f")
 
     sweeps_per_frame = st.sidebar.number_input(
@@ -74,36 +79,58 @@ def render_streamlit():
 
     sleep_timer = st.sidebar.number_input(
         "Additional sleep time between MC sweeps",
-        min_value = 0.0,
-        value = 0.0,
-        max_value = 60.0)
+        min_value=0.0,
+        value=0.0,
+        max_value=60.0)
 
     # Heuristic for making plot look reasonable
-    chart_rect_width = 500 // np.max([Nx,Ny])  
+    chart_rect_width = 500 // np.max([Nx, Ny])
     if chart_rect_width == 0:
         chart_rect_width = 1  # min width 1 px
+
+
+    while True:
+
+        # Reinitialize upon changing simulation size
+        if Nx != Ising.Nx or Ny != Ising.Ny:
+            Ising.reinitialize(Nx, Ny)
+
+        # If T changes, recompute beta
+        if Ising.beta != 1/T:
+            Ising.set_beta(1.0/T)
+
+        Ising.monte_carlo_sweep(sweeps_per_frame)
+
+        with chartholder.container():
+
+            chart = make_chart(chart_rect_width)
+            st.altair_chart(chart, use_container_width=True)
+
+            st.write(
+                f"Currently on MC sweep {Ising.sweeps}, "
+                    f"proceeding at {Ising.sweeps_per_second:0.1f} "
+                f"sweeps per second."
+                )
+            mean_energy = Ising.df['energy'].sum() \
+                / (Ising.Nx * Ising.Ny)
+            st.write(f"Mean energy is {mean_energy:.3f}.")
+
+            mean_spin = Ising.df['spin'].sum() \
+                / (Ising.Nx * Ising.Ny)
+            st.write(f"Mean spin is {mean_spin:.3f}.")
+
+            acc_rate = Ising.accepted_count \
+                / (Ising.accepted_count + Ising.rejected_count)
+            st.write(f"Monte Carlo acceptance rate {acc_rate:.3f}.")
+
+        if sleep_timer > 0:
+            time.sleep(sleep_timer)
 
     st.write(r'''
     Each Monte Carlo (MC) sweep is $N_x N_y$ random Monte Carlo moves applied to the simulation board.  Moves are accepted with probability $\exp(-E/kT)$ where units are set such that Boltzmann's constant $k=1$.  Adjust sweeps per frame or add sleep time between loop iterations if the simulation is outpacing streamlit's ability to redraw the Ising model.  Critical point for the ordered-disordered phase transition is near the default temperature of 2.27.
     ''')
 
-    while True:
-        # Reinitialize upon changing simulation size
-        if Nx != Ising.Nx or Ny != Ising.Ny:
-            Ising.reinitialize(Nx,Ny);
-        Ising.set_beta(1.0/T)
-
-        Ising.monte_carlo_sweep(sweeps_per_frame)
-
-        with chartholder.container():
-            chart = make_chart(chart_rect_width)
-            st.altair_chart(chart, use_container_width=True)
-            st.text("Currently on MC sweep {}, proceeding at {} sweeps per second.".format(Ising.sweeps, Ising.sweeps_per_second))
-            st.text("Mean energy is {:.5f}.".format(Ising.df['energy'].sum() / (Ising.Nx * Ising.Ny)))
-            st.text("Mean spin is {:.3f}.".format(Ising.df['spin'].sum() / (Ising.Nx * Ising.Ny)))
-            st.text("MC acceptance rate {}.".format(Ising.accepted_count / (Ising.accepted_count + Ising.rejected_count)))
-        if sleep_timer > 0:
-            time.sleep(sleep_timer)
 
 if __name__ == '__main__':
+
     render_streamlit()
--- ising_state.py
+++ ising_state.py
@@ -1,60 +1,69 @@
 import numpy as np
-from numpy.random import default_rng
 import pandas as pd
 import time
 
 
 class IsingState():
+
     Nx = 10
     Ny = 10
-    ist = None  # Ising board state
-    iste = None # Ising board energy
-    df = None   # dataframe used to plot in altair
-    beta = 1
-    exp_beta_e = dict()  # lookup table
+    spins = None     # Ising board state
+    energies = None  # Ising board energy
+    df = None        # dataframe used to plot in altair
+    beta = None      # 1/T (temperature)
+    exp_beta_e = dict()  # discrete lookup table
     sweeps = 0
     neighborlist = None  # precompute neighbors
     accepted_count = None
     rejected_count = None
-
     rng = np.random.default_rng()
 
-
     def __init__(self, Nx, Ny):
-        self.reinitialize(Nx,Ny)
+        self.reinitialize(Nx, Ny)
 
     def reinitialize(self, Nx, Ny):
+        if self.beta is None:
+            self.set_beta(1 / 2.2692)
         self.Nx = Nx
         self.Ny = Ny
-        self.spins = 2*self.rng.integers(0,2,size=(Nx,Ny))-1
-        self.energies = np.zeros(shape=(Nx,Ny),dtype=np.float64)
+        # Initialize spins to random +1, -1
+        self.spins = 2*self.rng.integers(0, 2, size=(Nx, Ny))-1
+        self.energies = np.zeros(
+            shape=(Nx, Ny),
+            dtype=np.float32
+            )
         self.sweeps = 0
         self.sweeps_per_second = 0
         self.accepted_count = 0
         self.rejected_count = 0
 
         # altair plot input needs pd dataframe
-        i,j = np.meshgrid(range(0,Nx),range(0,Ny))
+        i, j = np.meshgrid(range(0, Nx), range(0, Ny))
         self.df = pd.DataFrame(
-            {'i' : i.ravel(),
-             'j' : j.ravel(),
-             'spin' : self.spins.ravel(),
-             'energy' : self.energies.ravel()
-            })
+            {
+                'i': i.ravel(),
+                'j': j.ravel(),
+                'spin': self.spins.ravel(),
+                'energy': self.energies.ravel()
+            }
+            )
 
         self.precompute_neighborlist()
         self.compute_energy()
 
     def precompute_neighborlist(self):
-        nl = np.empty(shape=(self.Nx,self.Ny,4,2),dtype = np.int32)
+        nl = np.empty(
+            shape=(self.Nx, self.Ny, 4, 2),
+            dtype=np.int32
+            )
         for i in range(self.Nx):
             for j in range(self.Ny):
-                nij = np.empty(shape=(4,2),dtype=np.int32)
-                nij[0] = (i-1,j)
-                nij[1] = (i+1,j)
-                nij[2] = (i,j-1)
-                nij[3] = (i,j+1)
-                #fix periodic boundary conditions
+                nij = np.empty(shape=(4, 2), dtype=np.int32)
+                nij[0] = (i-1, j)
+                nij[1] = (i+1, j)
+                nij[2] = (i, j-1)
+                nij[3] = (i, j+1)
+                # fix periodic boundary conditions
                 for t in nij:
                     if t[0] < 0:
                         t[0] += self.Nx
@@ -75,56 +84,64 @@ class IsingState():
         self.df['energy'] = self.energies.ravel()
         return self.df
 
-    def set_beta(self,beta):
+    def set_beta(self, beta):
         self.beta = beta
-        #precompute exp(-beta*e) - dict lookup is faster in python
+        # precompute exp(-beta*e) - dict lookup is faster in python
         d = dict()
-        for i in [4,8]:  #Only possible energies for unbiased ising on square lattice
+        for i in [4, 8]:  # only possible delta E from flips
             d[i] = np.exp(-beta*i)
         self.exp_beta_e = d
 
     def monte_carlo_move(self):
-        #select site
-        row = self.rng.integers(0,self.Nx)
-        col = self.rng.integers(0,self.Ny)
-        neighbors = self.neighborlist[row][col]
-        neigh_spins = 0 #Sum spins of neighbors
 
-        #consider manual unrolling
+        # select site
+        row = self.rng.integers(0, self.Nx)
+        col = self.rng.integers(0, self.Ny)
+        neighbors = self.neighborlist[row][col]
+        neigh_spins = 0  # Sum spins of neighbors
+
+        # iterate over neighbors
         for t in neighbors:
-            neigh_spins += self.spins[t[0],t[1]]
-        spin = self.spins[row,col]
+            neigh_spins += self.spins[t[0], t[1]]
+        spin = self.spins[row, col]
 
         trial_delta_e = 2*spin*neigh_spins
 
+        # Favorable delta e, accept spin flip
         if trial_delta_e <= 0:
-            self.accepted_count+=1
-            self.spins[row,col]=-spin
+            self.accepted_count += 1
+            self.spins[row, col] = -spin
+        # Roll Monte Carlo move rand[0,1) vs exp(-beta*E)
+        #  energy is a lookup table over discrete values
+        #  of e.
         elif self.rng.random() < self.exp_beta_e[trial_delta_e]:
-            self.accepted_count+=1
-            self.spins[row,col]=-spin
+            self.accepted_count += 1
+            self.spins[row, col] = -spin
+        # Reject mc move (do nothing to state)
         else:
-            self.rejected_count+=1
+            self.rejected_count += 1
 
     def compute_energy(self):
+
         for row in range(self.Nx):
             for col in range(self.Ny):
                 neighbors = self.neighborlist[row][col]
-                neigh_spins = 0 #Sum spins of neighbors
+                neigh_spins = 0  # Sum spins of neighbors
                 for t in neighbors:
-                    neigh_spins += self.spins[t[0],t[1]]
-                spin = self.spins[row,col]
+                    neigh_spins += self.spins[t[0], t[1]]
+                spin = self.spins[row, col]
                 self.energies[row][col] = -spin*neigh_spins
 
-    def monte_carlo_sweep(self,sweeps=1):
+    def monte_carlo_sweep(self, sweeps=1):
+
         self.accepted_count = 0
         self.rejected_count = 0
         tstart = time.time()
         for _ in range(0, self.Nx * self.Ny * sweeps):
             self.monte_carlo_move()
-        self.sweeps+=sweeps
+        self.sweeps += sweeps
         self.compute_energy()
         self.sweeps_per_second = sweeps / (time.time() - tstart)
 
 
-Ising = IsingState(3,3)
+Ising = IsingState(3, 3)
